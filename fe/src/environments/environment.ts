d7981623bdc2937fbd0eec3ac8d5d711e7437652
Gokulmty-Work <gklmoorthy.work@gmail.com>
2025-03-01 22:39:16 +0530

    environment change

--- fe/src/environments/environment.ts
+++ fe/src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: true,
     apiUrl: 'https://genai2024.onrender.com',
+    serviceNowUrl: 'https://dev221653.service-now.com/api/now/table',
+
     // apiUrl: 'http://localhost:3000/api',
     baseHref: '/',
     version: '1.0.0',
